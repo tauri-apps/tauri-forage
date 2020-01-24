aed96bbefe0569b4f7cac203d5bc88bd365ca30b
Daniel Thompson-Yvetot <denjell@sfosc.org>
2020-01-24 12:09:34 +0100

    chore(dist): remove dist

--- dist/src/index.d.ts
+++
@@ -1,5 +0,0 @@
-import { cryptoForage } from './cryptoForage';
-import { forage } from './curriedForage';
-import { crypto } from './cryptoPrimitives';
-import { handler } from './handler';
-export { forage, cryptoForage, handler, crypto };
